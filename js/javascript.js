f300c27cf20a9db1383c4e5430a0f90d6be23ff8
Nedaaaljezani <nedaa.aljezani@yukonu.ca>
2023-04-05 17:09:25 -0600

    update

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,17 @@
-$(document).ready(function() {
-  $('.experiment-image-1').click(function() {
+$(document).ready(function () {
+  $('.experiment-image-1').click(function () {
     $('.experiment-image-2').fadeToggle("slow");
     // Alternative animation for example
     // slideToggle("fast");
   });
 });
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursoroutline = document.querySelector("[data-cursor-outline]");
+window.addEventListener("mousemove", function(e){
+
+  const posX = e.clientX;
+  const posy = e.clientY;
+
+  cursorDot.getElementsByClassName.left = `${posX}px`;
+
+})
